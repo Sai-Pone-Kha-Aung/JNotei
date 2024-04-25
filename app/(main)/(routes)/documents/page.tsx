aa56914c080385421cd85bbdf47838aa826d9f52
Sai Pone Kha Aung <saiponekhaaaung@gmail.com>
2024-04-25 12:58:08 +0700

    Add the user dropdown

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 const DocumentsPage = () => {
+    const { user } = useUser();
     return (
-        <div>
-            Documents
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image
+                src="/empty.png"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="dark:hidden"
+            />
+            <Image
+                src="/empty-dark.png"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="hidden dark:block"
+            />
+
+            <h2>
+                Welcome to {user?.firstName}&apos;s JNotei
+            </h2>
+            <Button>
+                <PlusCircle className="w-4 h-4 mr-2"/>
+                Create a note
+            </Button>
         </div>
     )
 }
