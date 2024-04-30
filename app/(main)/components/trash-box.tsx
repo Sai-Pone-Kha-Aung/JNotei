6533bfb72dac59e0b98bdd97f0c8b18174cf5625
Sai Pone Kha Aung <saiponekhaaaung@gmail.com>
2024-04-30 15:25:22 +0700

    add the settings function

--- app/(main)/components/trash-box.tsx
+++ app/(main)/components/trash-box.tsx
@@ -100,13 +100,13 @@ export const TrashBox  = () => {
                         <div 
                             onClick={(e) => onRestore(e, doc._id)}
                             role='button'
-                            className='rounded-sm p-2 hover:bg-neutral-200'>
+                            className='rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600'>
                             <Undo className='w-4 h-4 text-muted-foreground'/>
                         </div>
                         <ConfirmModal onConfirm={() => onDelete(doc._id)}>
                         <div
                             role='button'
-                            className='rounded-sm p-2 hover:bg-neutral-200'
+                            className='rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600'
                         >
                             <Trash className='w-4 h-4 text-muted-foreground'/>
                         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProviders } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,7 @@ export default function RootLayout({
 
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="notei-theme-2">
           <Toaster position="bottom-center"/>
+          <ModalProvider/>
           {children}
         </ThemeProvider>
         </ConvexClientProviders>
